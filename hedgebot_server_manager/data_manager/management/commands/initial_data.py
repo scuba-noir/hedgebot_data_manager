269bf3d3a5eb24303953e13d310307e1ea3e6a22
Chris Thompson <111588659+scuba-noir@users.noreply.github.com>
2023-02-06 16:38:00 -0500

    Update initial_data.py

--- hedgebot_server_manager/data_manager/management/commands/initial_data.py
+++ hedgebot_server_manager/data_manager/management/commands/initial_data.py
@@ -8,4 +8,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         data_df = pd.read_csv('Final_Final_MC.csv', index_col=False)
-        print(data_df)
+        data_df['sim_date'] = pd.to_datetime(data_df['sim_date'])
+        data_df['end_date'] = pd.to_datetime(data_df['end_date'])
+        data_df['forecast_period'] = pd.to_datetime(data_df['forecast_period'])
+        
+        for row, items in data_df.iterrows():
+            temp_ref = items['reference']
+            temp_sim_date = items['sim_date']
+            temp_f_period = items['forecast_period']
+            temp_mu = items['mean_returned']
+            temp_sigma = items['std_returned']
+            temp_end_date = items['end_date']
+
+            obj = models.monte_carlo_market_data.objects.update_or_create(simulation_date = temp_sim_date, forecast_period=temp_f_period, reference=temp_ref, mean_returned=temp_mu, std_returned=temp_sigma, end_date=temp_end_date)
