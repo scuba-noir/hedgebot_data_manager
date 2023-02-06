b21e0fd5faa7864e1e59d9f8bc81f14d2be938c6
Chris Thompson <111588659+scuba-noir@users.noreply.github.com>
2023-02-06 16:22:14 -0500

    script

--- hedgebot_server_manager/data_manager/initial_data.py
+++
@@ -1,6 +0,0 @@
-from hedgebot_server_manager.data_manager import models
-import pandas as pd
-
-
-data_df = pd.read_csv('Final_Final_MC.csv', index_col=False)
-print(data_df)
---
+++ hedgebot_server_manager/scripts/initial_data.py
@@ -0,0 +1,6 @@
+from hedgebot_server_manager.data_manager import models
+import pandas as pd
+
+def run():
+    data_df = pd.read_csv('Final_Final_MC.csv', index_col=False)
+    print(data_df)
