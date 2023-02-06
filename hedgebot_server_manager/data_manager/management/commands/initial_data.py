60fdebe81cb18c6ad91b4c28b366d39de4f1d68f
Chris Thompson <111588659+scuba-noir@users.noreply.github.com>
2023-02-06 17:58:48 -0500

    rr

--- hedgebot_server_manager/data_manager/management/commands/initial_data.py
+++ hedgebot_server_manager/data_manager/management/commands/initial_data.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from data_manager import models
-from data_manager.models import monte_carlo_market_data
+from data_manager.models import monte_carlo_market_data, market_data
 import pandas as pd
 
 
@@ -25,7 +25,8 @@ class Command(BaseCommand):
                 temp_end_date = items['end_date']
 
                 obj = models.monte_carlo_market_data.objects.update_or_create(simulation_date = temp_sim_date, forecast_period=temp_f_period, reference=temp_ref, mean_returned=temp_mu, std_returned=temp_sigma, end_date=temp_end_date)
-        else:
+            else:
+                """
                 relevant_factors = ['sugar_1', 'hydrous', 'anhydrous', 'usdbrl']
                 max_date = monte_carlo_market_data.objects.latest('simulation_date').simulation_date
                 current_mc_data_df = pd.DataFrame(monte_carlo_market_data.objects.filter(reference__in = relevant_factors).filter(simulation_date = max_date).values())
@@ -36,12 +37,12 @@ class Command(BaseCommand):
                 current_mc_data_df['std_returned'] = current_mc_data_df['std_returned'].div(current_mc_data_df['mean_returned'])
                 current_mc_data_df['pct_change'] = current_mc_data_df.groupby('factor_label')['mean_returned'].pct_change().fillna(0)
                 print(current_mc_data_df)
+                """
+                i = 9
+
         if emp_2 == True:
-            data_df = pd.read_csv('temp_price_csv.csv')
-            temp_cols = data_df.columns
-            for cols in temp_cols:
-                if cols == 'date':
-                    continue
-                temp_ref = cols
-                temp_df = data_df.filter([temp_ref, 'date'])
-                print(temp_df)
+            data_df = pd.read_csv('temp_market_csv.csv', index_col=False)
+            data_df = data_df.drop(['id']).drop_duplicates()
+            data_df['date'] = pd.to_datetime(data_df['date'])
+            for row, items in data_df:
+                obj = market_data.objects.update_or_create(ticker=items['ticker'], date=items['date'], value=items['value'], units=items['units'])
