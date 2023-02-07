4cc89d2a0b02f4b2d8e32aa2319946e9d2e69c68
Chris Thompson <111588659+scuba-noir@users.noreply.github.com>
2023-02-07 06:18:08 -0500

    Update views.py

--- hedgebot_server_manager/data_manager/views.py
+++ hedgebot_server_manager/data_manager/views.py
@@ -24,7 +24,7 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from data_manager.serializers import SugarPositionSerializers, MonteCarloDataSerializer, MarketDataSerializer, FinSimMetaDataSerializer, UserListSerializers
 
-import full_simulation_run
+from management.commands import full_simulation_run
 
 def findnth(string, substring, n):
    parts = string.split(substring, n + 1)
