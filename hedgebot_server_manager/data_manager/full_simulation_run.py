3e9eb1e17a5bd13fe52806ca4f17f740c2959335
Chris Thompson <111588659+scuba-noir@users.noreply.github.com>
2023-02-07 10:36:52 -0500

    Update full_simulation_run.py

--- hedgebot_server_manager/data_manager/full_simulation_run.py
+++ hedgebot_server_manager/data_manager/full_simulation_run.py
@@ -142,7 +142,6 @@ def main(initial_simulation_variables, prev_year_financial_df, mc_meta_data_1):
         financial_indices_final_df = pd.DataFrame(temp_financial_indices_df)
 
         final_df = simulate_statements.aggregate_fin_sim_results(income_statement_final_df, cash_flow_final_df, assets_final_df, liabilities_final_df, financial_indices_final_df)
-        final_df.to_csv('final_sim_output.csv')
         final_df['Mean Returned'] = pd.to_numeric(final_df['Mean Returned'].astype(str).str.replace(',',''), errors='coerce').fillna(0).astype(int)
 
         temp_dict = {
