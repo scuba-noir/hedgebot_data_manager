a11d8e3a0675e6082e719cd34b326d311a4c8d1b
Chris Thompson <111588659+scuba-noir@users.noreply.github.com>
2023-02-08 18:40:01 -0500

    Update views.py

--- hedgebot_server_manager/data_manager/views.py
+++ hedgebot_server_manager/data_manager/views.py
@@ -278,6 +278,7 @@ def historical_mc_data_api(request):
 
     temp_date = date.today() + relativedelta(months=-6)
     forecast_date = monte_carlo_market_data.objects.latest('forecast_period').forecast_period
+    print(forecast_date)
     data = monte_carlo_market_data.objects.filter(simulation_date__gte = temp_date).filter(forecast_period__gte = forecast_date)
     serializer = HistMCDataSerializer(data, context={'request':request}, many=True)
     return Response(serializer.data)
