ab0bbcb8ba8198404e88ac7a35fdeff51fe240dd
Chris Thompson <111588659+scuba-noir@users.noreply.github.com>
2023-02-08 20:50:36 -0500

    Update views.py

--- hedgebot_server_manager/data_manager/views.py
+++ hedgebot_server_manager/data_manager/views.py
@@ -277,7 +277,7 @@ def risk_management_table_api(request):
         print('---------------')
         
         max_date = current_expectations.latest('date').date
-        current_expectations = pd.DataFrame.from_dict(current_expectations.filter(date = max_date).values()[0])
+        current_expectations = pd.DataFrame.from_dict(current_expectations.filter(date = max_date).values())
 
         relevent_sim_variables = ['sugar_price','hydrous_price','anhydrous_price','fx_rate','sugar_revenues','hydrous_revenues','anhydrous_revenues','cogs', 'gross_profit','sga_costs','ebit','financial_costs','net_income']
         
