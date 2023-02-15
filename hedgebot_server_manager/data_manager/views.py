5e85c5ac2dc9b41741de364db145fb20da25b7d3
Chris Thompson <111588659+scuba-noir@users.noreply.github.com>
2023-02-14 23:53:44 -0500

    Update views.py

--- hedgebot_server_manager/data_manager/views.py
+++ hedgebot_server_manager/data_manager/views.py
@@ -42,11 +42,10 @@ def findnth(string, substring, n):
 def return_current_season_df(username):
     
     print(username)
-    current_season_df = user_forecasts_assumptions_results.objects.filter(username = username).filter(season='2023_24').last()
+    current_season_df = user_forecasts_assumptions_results.objects.filter(username = username).filter(season='2023_24')
     verbose_name_dict = user_forecasts_assumptions_results.return_verbose(user_forecasts_assumptions_results)
-    print(current_season_df.values)
     max_sim_date = current_season_df.latest('date').date
-    current_season_df = current_season_df.filter(date = max_sim_date)
+    current_season_df = current_season_df.filter(date = max_sim_date).first()
     current_season_df = pd.DataFrame(current_season_df.values())
     current_season_df['date'] = pd.to_datetime(current_season_df['date'])
     counter = 0
