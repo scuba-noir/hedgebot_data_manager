e6bde3e1c53693b89e3cfb00a39c5e033aaa2ad0
Chris Thompson <111588659+scuba-noir@users.noreply.github.com>
2023-02-14 23:56:12 -0500

    Update views.py

--- hedgebot_server_manager/data_manager/views.py
+++ hedgebot_server_manager/data_manager/views.py
@@ -46,6 +46,8 @@ def return_current_season_df(username):
     verbose_name_dict = user_forecasts_assumptions_results.return_verbose(user_forecasts_assumptions_results)
     max_sim_date = current_season_df.latest('date').date
     current_season_df = current_season_df.filter(date = max_sim_date)
+    max_id = current_season_df.latest('id').id
+    current_season_df = current_season_df.filter(id = max_id)
     print(current_season_df.values())
     current_season_df = pd.DataFrame(current_season_df.values())
     current_season_df['date'] = pd.to_datetime(current_season_df['date'])
