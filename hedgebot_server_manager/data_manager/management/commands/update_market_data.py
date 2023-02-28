69f6ee5a60787bf65c6141186137562518d0e842
Chris Thompson <111588659+scuba-noir@users.noreply.github.com>
2023-02-28 11:22:55 -0500

    Update update_market_data.py

--- hedgebot_server_manager/data_manager/management/commands/update_market_data.py
+++ hedgebot_server_manager/data_manager/management/commands/update_market_data.py
@@ -8,6 +8,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        old_data = pd.DataFrame(market_data.objects.all().values())
+        all_tickers = old_data['ticker'].unique()
+        old_data_df = pd.DataFrame(columns=['id','date','ticker','value','units'])
+        for ticker in all_tickers:
+            temp_max_date = max(old_data['date'].loc[old_data['ticker'] == ticker])
+            temp_row = old_data.loc[(old_data['ticker'] == ticker) & (old_data['date'] == temp_max_date)]
+            old_data_df = pd.concat([old_data_df, temp_row])
+
+        print(old_data_df)
+
+
         ticker_ls = ['SB1 Comdty','USDBRL Curncy','BAAWHYDP Index','BAAWANAB Index']
         db = pymysql.connect(host = 'database-1.c8dbzf9wtrjo.us-east-2.rds.amazonaws.com', user = 'admin', password = 'Ktr321ugh!')
         cursor = db.cursor()
@@ -25,6 +36,7 @@ class Command(BaseCommand):
             '''
 
         data_df = pd.read_sql(sql = sql, con = db)
+        db.close()
         data_df = data_df.loc[data_df['ticker'].isin(ticker_ls)]
 
         print(data_df)
