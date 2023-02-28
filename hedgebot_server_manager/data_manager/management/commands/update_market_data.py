531418f3c807bb40611fc1844c180af85ae71f98
Chris Thompson <111588659+scuba-noir@users.noreply.github.com>
2023-02-28 13:25:44 -0500

    Update update_market_data.py

--- hedgebot_server_manager/data_manager/management/commands/update_market_data.py
+++ hedgebot_server_manager/data_manager/management/commands/update_market_data.py
@@ -14,7 +14,6 @@ class Command(BaseCommand):
         cursor = db.cursor()
         sql = '''use collateral_prices'''
         cursor.execute(sql)
-
         old_data = pd.DataFrame(market_data.objects.all().values())
         old_data['date'] = pd.to_datetime(old_data['date'])
         all_tickers = ['SBMAY1 Comdty', 'SBJUL1 Comdty', 'SBOCT1 Comdty', 'SBMAR2 Comdty', 'GCFPUBGC Index', 'CL1 Comdty', 'BZCESECA Index', 'SBMAR1 Comdty','SB1 Comdty', 'USDBRL Curncy', 'BAAWHYDP Index', 'BAAWANAB Index']
@@ -23,7 +22,6 @@ class Command(BaseCommand):
         if check_entries_bool == True:
 
             obj_delete = market_data.objects.exclude(ticker__in = labels_ls)
-            print(pd.DataFrame(obj_delete.values())['ticker'].unique().tolist())
             obj_delete.delete()
         
         sql = '''SELECT * from market_data_prices'''
@@ -36,29 +34,9 @@ class Command(BaseCommand):
             temp_data_df = full_market_prices_df.loc[(full_market_prices_df['ticker'] == ticker) & (full_market_prices_df['date'] > temp_max_date)]
             temp_data_df = temp_data_df.drop_duplicates(subset = 'date')
             for row, items in temp_data_df.iterrows():
-                print(items)
                 try:
                    market_data.objects.get_or_create(ticker = items.ticker, value = items.value, units = items.units, date = items.date)
                 except:
                     continue
             
-            
-
-
         
-        """    
-        columns = ['Id','Ticker', 'Description', 'Origin', 'Dashboard', 'Units', 'Date','Value','Most Recent']
-        sql = '''
-            WITH list AS (
-                SELECT m.*, ROW_NUMBER() OVER (PARTITION BY ticker ORDER BY date DESC) AS rn
-                FROM market_data_prices as m
-            )
-            SELECT * FROM list where rn = 1;
-            '''
-
-        data_df = pd.read_sql(sql = sql, con = db)
-        db.close()
-        data_df = data_df.loc[data_df['ticker'].isin(ticker_ls)]
-
-        print(data_df)
-        """
